3898ef60aec97fb2208f7c542c6eeeba848f7f43
LakeYS <Lake5Gaming1531@gmail.com>
2016-07-14 15:26:05 -0400

    Fixed tools getting stuck

--- Server_Anti_Rapid_Fire/server.cs
+++ Server_Anti_Rapid_Fire/server.cs
@@ -1,17 +1,29 @@
+deactivatePackage("antiRapidFire");
 package antiRapidFire 
 {
 	function serverCmdUseTool(%client,%t)
 	{
 		// This version is less glitchy but it uses schedules
-		if(isEventPending(%client.antiRapidFireSched))
+		
+		if(%client.antiRapidFireCheck)
 		{
-			cancel(%client.antiRapidFireSched);
-			%client.antiRapidFireSched = schedule(16,0,antiRapidFireCheck,%client,%t);
+			%client.antiRapidFireCheck = 0;
+			Parent::serverCmdUseTool(%client,%t);
 		}
 		else
 		{
-			%client.antiRapidFireSched = schedule(16,0,antiRapidFireCheck,%client,%t);
-			Parent::serverCmdUseTool(%client,%t);
+			if(isEventPending(%client.antiRapidFireSched) && !%client.antiRapidFireCheck)
+			{
+				cancel(%client.antiRapidFireSched);
+				%client.antiRapidFireSched = schedule(32,0,antiRapidFireCheck,%client,%t);
+				%client.antiRapidFireCheck = 0;
+			}
+			else
+			{
+				cancel(%client.antiRapidFireSched);
+				%client.antiRapidFireSched = schedule(32,0,antiRapidFireCheck,%client,%t);
+				%client.antiRapidFireCheck = 0;
+			}
 		}
 		
 		//if(getSimTime()-%client.antiRapidFire >= 64 || !%client.antiRapidFire) 
@@ -23,8 +35,11 @@ package antiRapidFire
 	
 	function antiRapidFireCheck(%client,%t)
 	{
-		if(!isEventPending(%client.antiRapidFireSched))
+		if(!isEventPending(%client.antiRapidFireSched) && !%client.antiRapidFireCheck)
+		{
+			%client.antiRapidFireCheck = 1;
 			serverCmdUseTool(%client,%t);
+		}
 	}
 };
 activatePackage("antiRapidFire");
