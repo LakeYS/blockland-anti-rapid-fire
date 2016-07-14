d0763a1b9861808762d345920fab825c95fa1dab
LakeYS <Lake5Gaming1531@gmail.com>
2016-07-13 23:33:31 -0400

    Fixed tool scrolling lag

--- Server_Anti_Rapid_Fire/server.cs
+++ Server_Anti_Rapid_Fire/server.cs
@@ -1,12 +1,30 @@
 package antiRapidFire 
 {
-	function serverCmdUseTool(%client,%t) 
-	{ 
-		if(getSimTime()-%client.antiRapidFire >= 64 || !%client.antiRapidFire) 
-		{ 
-			Parent::serverCmdUseTool(%client,%t); 
-			%client.antiRapidFire=getsimtime();
+	function serverCmdUseTool(%client,%t)
+	{
+		// This version is less glitchy but it uses schedules
+		if(isEventPending(%client.antiRapidFireSched))
+		{
+			cancel(%client.antiRapidFireSched);
+			%client.antiRapidFireSched = schedule(16,0,antiRapidFireCheck,%client,%t);
 		}
+		else
+		{
+			%client.antiRapidFireSched = schedule(16,0,antiRapidFireCheck,%client,%t);
+			Parent::serverCmdUseTool(%client,%t);
+		}
+		
+		//if(getSimTime()-%client.antiRapidFire >= 64 || !%client.antiRapidFire) 
+		//{ 
+		//	Parent::serverCmdUseTool(%client,%t); 
+		//	%client.antiRapidFire=getsimtime();
+		//}
+	}
+	
+	function antiRapidFireCheck(%client,%t)
+	{
+		if(!isEventPending(%client.antiRapidFireSched))
+			serverCmdUseTool(%client,%t);
 	}
 };
 activatePackage("antiRapidFire");
